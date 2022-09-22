ddd50197797034d4021c9116a0957f3ea64678bf
Saima Teasha <74995287+teashas@users.noreply.github.com>
2022-09-22 15:09:12 -0700

    Delete Spawning and Concurrent Execution of Processes directory

--- Spawning and Concurrent Execution of Processes/child2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <unistd.h>
-
-int main()
-{
-    printf("Process #%d, now running program 'child2.exe' My parent's PID is %d\n", getpid(), getppid());
-}
